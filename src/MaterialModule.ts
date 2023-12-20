f45a70f43f0978dfdc5aee6baa992bd3314815ea
robert.e.enloe.ctr <robert.e.enloe.ctr@socom.mil>
2023-12-19 19:15:13 -0500

    badge

--- src/MaterialModule.ts
+++ src/MaterialModule.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   exports: [
@@ -16,6 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
+    MatBadgeModule,
   ],
 })
 export class MaterialModule {}
--- src/app/component/menubar/menubar.component.ts
+++ src/app/component/menubar/menubar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
-  styleUrls: ['./menubar.component.css']
+  styleUrls: ['./menubar.component.css'],
 })
 export class MenubarComponent {
-
+  badgeHidden = false;
+  badgeVisibility() {
+    this.badgeHidden = true;
+  }
 }
